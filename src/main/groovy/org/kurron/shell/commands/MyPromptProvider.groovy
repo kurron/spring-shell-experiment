68fcde9875b0ff238709377a8ac31b1adff0f56b
Ron Kurr <kurr@kurron.org>
2013-11-03 14:39:18 -0500

    Got a custom prompt working.

---
+++ src/main/groovy/org/kurron/shell/commands/MyPromptProvider.groovy
@@ -0,0 +1,22 @@
+package org.kurron.shell.commands
+
+import org.springframework.core.Ordered
+import org.springframework.core.annotation.Order
+import org.springframework.shell.plugin.support.DefaultPromptProvider
+import org.springframework.stereotype.Component
+
+@SuppressWarnings( 'GroovyUnusedDeclaration' )
+@Component
+@Order( Ordered.HIGHEST_PRECEDENCE )
+public class MyPromptProvider extends DefaultPromptProvider {
+
+    @Override
+    public String getPrompt() {
+        'hw-shell>'
+    }
+
+    @Override
+    String name() {
+        "My prompt provider "
+    }
+}
