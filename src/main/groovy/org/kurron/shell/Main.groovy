318fe3f3ea992130895ced4c088409f773320f2d
Ron Kurr <kurr@kurron.org>
2013-11-03 14:14:33 -0500

    Got a bare bones version working.

---
+++ src/main/groovy/org/kurron/shell/Main.groovy
@@ -0,0 +1,12 @@
+package org.kurron.shell
+
+import org.springframework.shell.Bootstrap
+
+/**
+ * Command-line launcher for the shell.
+ */
+class Main {
+    public static void main ( String[] args ) {
+        Bootstrap.main( args )
+    }
+}
